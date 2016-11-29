456446cd1d0363a47fadd5d480f441f12f5a70cb
Zack Sheppard <z@zack.io>
2016-11-29 16:51:34 -0500

    Updates so there is only one DOM change. The code needs some cleanup but it is working with one DOM update.

--- filter.js
+++ filter.js
@@ -3,20 +3,40 @@
 const betteridgeRegEx = 
   /(^|\.\s|\-\s|—\s|:\s)(do|does|did|can|could|was|will|is|has).+\?/i;
 
-const tweets = document.getElementsByClassName('tweet-text');
-const tweetsArray = Array.prototype.slice.call(tweets);
 
-function findAncestor (el, className) {
+function findAncestor(el, className) {
   while ( (el = el.parentElement) && !el.classList.contains(className) );
   return el;
 }
 
-for (var i = 0; i < tweetsArray.length; i++) {
-  let text = tweetsArray[i].textContent;
+function updateStream(containerElement) {
+  let filteredElement = containerElement;
 
-  if ( betteridgeRegEx.test(text) ) {
-    let tweet = findAncestor(tweetsArray[i], 'stream-item');
-    tweet.style.opacity = "0.3";
+  let tweets = filteredElement.getElementsByClassName('tweet-text');
+  // let tweetsArray = Array.prototype.slice.call(tweets);
+
+  for (var i = 0; i < tweets.length; i++) {
+    let text = tweets[i].textContent;
+
+    if ( betteridgeRegEx.test(text) ) {
+      let tweet = findAncestor(tweets[i], 'stream-item');
+      tweet.style.opacity = "0.3";
+
+      if (i === tweets.length -1 ) {
+        containerElement.parentNode.replaceChild(filteredElement, containerElement);
+      }
+    } else {
+      if (i === tweets.length -1 ) {
+        containerElement.parentNode.replaceChild(filteredElement, containerElement);
+      }
+    }
+
+    
   }
-  
 }
+
+// save .stream-container to a variable
+var streamContainer = document.getElementById('timeline');
+
+updateStream(streamContainer);
+
