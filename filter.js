654da57e4e5382cc87c963abe6c161ff73809a32
Zack Sheppard <z@zack.io>
2016-11-28 19:09:20 -0500

    betteridge's law filter is working

--- filter.js
+++ filter.js
@@ -1,5 +1,5 @@
 var betteridgeRegEx = 
-  /(^|\.\s|\-\s|—\s)(do|does|did|can|could|was|will|is).+\?/ig;
+  /(^|\.\s|\-\s|—\s|:\s)(do|does|did|can|could|was|will|is).+\?/i;
 
 var tweets = document.getElementsByClassName('tweet-text');
 var tweetsArray = Array.prototype.slice.call(tweets);
@@ -12,14 +12,9 @@ function findAncestor (el, className) {
 for (var i = 0; i < tweetsArray.length; i++) {
   var text = tweetsArray[i].textContent;
 
-  if (betteridgeRegEx.test(text) ) {
-    var tweet = findAncestor(tweets[i], 'stream-item');
-    console.log('>>>>>>>>'  + i + '<<<<<<<<<<');
-    console.log(text);
-    tweet.style.opacity = "0.5";
+  if ( betteridgeRegEx.test(text) ) {
+    var tweet = findAncestor(tweetsArray[i], 'stream-item');
+    tweet.style.opacity = "0.3";
   }
+  
 }
-
-
-// NOTES
-// try both element.innerText and element.textContent
