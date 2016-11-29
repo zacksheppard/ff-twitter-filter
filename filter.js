3a83f838b9850be96a9efd0c7a5362fb5be047d9
Zack Sheppard <z@zack.io>
2016-11-29 13:08:36 -0500

    add comment for regex

--- filter.js
+++ filter.js
@@ -1,5 +1,7 @@
+// Quick and dirty regex but a good starting point. It catches about 90% of 
+// applicable tweets and has false positives about 5% of the time
 const betteridgeRegEx = 
-  /(^|\.\s|\-\s|—\s|:\s)(do|does|did|can|could|was|will|is).+\?/i;
+  /(^|\.\s|\-\s|—\s|:\s)(do|does|did|can|could|was|will|is|has).+\?/i;
 
 const tweets = document.getElementsByClassName('tweet-text');
 const tweetsArray = Array.prototype.slice.call(tweets);
